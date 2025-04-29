83d8a34c0f3bc7f5862ad371d5e0c6152d84f31b
Klaus Paul <klaus.op@icloud.com>
2025-04-28 20:15:39 -0400

    Initial commit for main file, adds gitignore, update casing of text in the README

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This Program Reads A List Of Names From A Text File, Randomly Picks One, And Displays It. It'S Perfect For Practicing File Handling And Working With Lists In Python.
+This program reads a list of names from a text file, randomly picks one, and displays it. It's perfect for practicing file handling and working with lists in Python.
---
+++ src/word-picker-from-file.py
@@ -0,0 +1,20 @@
+import os
+
+
+def cls():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def main():
+    """
+    main Reads a list of names from a text file, randomly picks one, and displays it.
+    It's perfect for practicing file handling and working with lists in Python.
+
+    Returns:
+        Nothing
+    """
+
+
+if __name__ == "__main__":
+    cls()
+    main()
